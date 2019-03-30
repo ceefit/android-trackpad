5f98923c4c7edcc70af8ceea425ddbe1f22d92ba
ceefit <ceefit@protonmail.com>
2019-03-30 15:59:28 -0400

    Implement trackpad-like panning of the cursor

--- android-trackpad-server.py
+++ android-trackpad-server.py
@@ -4,16 +4,36 @@ from aiohttp import web
 
 pyautogui.PAUSE = 0
 
+offset_x = offset_y = None
+scaling_factor = 2
+
 
 async def websocket_handler(request):
+
+    global offset_x
+    global offset_y
 
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
-            x, y = msg.data.split('x')
-            pyautogui.moveTo((round(float(x), 0)), round(float(y), 0), 0)
+            if msg.data.startswith('letoff'):
+                offset_x = offset_y = None
+            else:
+                x, y = msg.data.split('x')
+                x = round(float(x), 0)
+                y = round(float(y), 0)
+
+                if offset_x is None or offset_y is None:
+                    offset_x = x
+                    offset_y = y
+                else:
+                    delta_x = (x - offset_x) * scaling_factor
+                    delta_y = (y - offset_y) * scaling_factor
+                    pyautogui.moveRel(delta_x, delta_y)
+                    offset_x = x
+                    offset_y = y
 
         elif msg.type == aiohttp.WSMsgType.ERROR:
             print('ws connection closed with exception %s' %
